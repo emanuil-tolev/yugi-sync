8fa5df3471c1da47d493fa27108b7c5a43bc7aed
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-10-11 14:55:57 +0100

    update .gitignore

---
+++ yugisync.py
@@ -0,0 +1,18 @@
+__author__ = 'Emanuil Tolev'
+
+
+def export():
+    # Copy the system files from common
+    pass
+
+
+def restore():
+    pass
+
+
+def push():
+    pass
+
+
+def pull():
+    pass
