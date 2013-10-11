9ad8c558911431496105ad67a75bbb0f67b227e6
Emanuil Tolev <emanuil.tolev@gmail.com>
2013-10-11 16:59:30 +0100

    planning it out

--- yugisync.py
+++ yugisync.py
@@ -1,12 +1,23 @@
+"""
+Usage:
+"""
 __author__ = 'Emanuil Tolev'
 
 
 def export():
-    # Copy the system files from common
+    # Copy the whole Common dir to the backup git repo
+    # copy yugi, kaiba and joey deck to backup repo
+    # export registry keys (be mindful of 64 vs 32 bit systems) to
+    #   backup repo
     pass
 
 
 def restore():
+    backup_local()
+    # delete current Common dir
+    # restore Common dir from backup repo
+    # restore decks from backup repo
+    # import registry keys
     pass
 
 
@@ -15,4 +26,10 @@ def push():
 
 
 def pull():
+    pass
+
+
+def backup_local():
+    # copy current Common dir to "OLD "
+    # copy yugi,kaiba,joey decks to .old
     pass
